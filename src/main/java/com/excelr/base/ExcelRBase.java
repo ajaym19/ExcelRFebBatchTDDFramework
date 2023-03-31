65fee4e5139b6709327de51751a1e99be651f67b
Ajay <ajay191987@yahoo.in>
2023-03-31 22:17:30 +0530

    code

--- src/main/java/com/excelr/base/ExcelRBase.java
+++ src/main/java/com/excelr/base/ExcelRBase.java
@@ -1,6 +1,10 @@
 package com.excelr.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,15 +12,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ExcelRBase {
 
 	public static WebDriver driver;
+	public static Properties prop;
 
 	public void initialization() {
+		readPropertyFile();
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.get(prop.getProperty("url"));
 	}
 
 	public void teardown() {
 		driver.close();
+	}
+
+	public void readPropertyFile() {
+		prop = new Properties();
+		String configFile = "./src/main/java/com/excelr/config/config.properties";
+		try {
+			
+			FileInputStream fis = new FileInputStream(configFile);
+			prop.load(fis);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
